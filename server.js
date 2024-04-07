8950a0a55f3559e154f6513538d250abefdceeba
erravamsi99 <e.vamsi9988@gmail.com>
2024-04-07 23:59:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -87,28 +87,3 @@ const TransactionSchema = new mongoose.Schema({
 
 module.exports = mongoose.model('Transaction', TransactionSchema);
 
-// App.js (React Frontend)
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
-function App() {
-  const [transactions, setTransactions] = useState([]);
-
-  useEffect(() => {
-    const fetchTransactions = async () => {
-      const res = await axios.get('/api/transactions');
-      setTransactions(res.data);
-    };
-
-    fetchTransactions();
-  }, []);
-
-  return (
-    <div>
-      <h1>FunFin: Your AI-Powered Personal Finance Planner</h1>
-      {/* Transaction list and input components go here */}
-    </div>
-  );
-}
-
-export default App;
